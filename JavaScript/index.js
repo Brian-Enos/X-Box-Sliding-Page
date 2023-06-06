282ffc415637dca61708a35217cbc4e57c530b56
Brian-Enos <brianenos9198@gmail.com>
2023-06-06 17:45:43 +0300

    Added event listeners to the left classes as well as their respective functions

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,4 +1,16 @@
 
-const left = document.querySelector('.left')
-const right = document.querySelector('.right')
-const container = document.querySelector('.main-container')
+const left = document.querySelector('.left');
+const right = document.querySelector('.right');
+const container = document.querySelector('.main-container');
+
+left.addEventListener('mouseenter', handleMouseEnter);
+
+function handleMouseEnter() {
+container.classList.add('hover-left');
+}
+
+left.addEventListener('mouseleave', handleMouseLeave);
+
+function handleMouseLeave() {
+container.classList.remove('hover-left');
+}
